922b607b6aac994097dc31f9cd88bf270f1b03f4
Coders Key <designer.afride@gmail.com>
2022-08-28 21:44:06 +0600

    input upadet

--- assets/Screens/Information.js
+++ assets/Screens/Information.js
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput, View, Button } from 'react-native'
+import { StyleSheet, TextInput, View, Button, Pressable, Image } from 'react-native'
 import React from 'react'
 import Text from '../components/text/text'
 import { colors, spacing } from '../components/theme'
@@ -9,22 +9,30 @@ export default function Information() {
         <Text preset="h3">Employee information</Text>
 
         <View style={[styles.card, styles.shadowProp]}>
-          <Text preset="paragraph">Leave type</Text>
-          <TextInput style={styles.InputBox} placeholder="Type your location"></TextInput>
+            <Text preset="paragraph">Leave type</Text>
+            <TextInput style={styles.InputBox} placeholder="Type your location"></TextInput>
 
-          <Text preset="paragraph">From Date</Text>
-          <TextInput style={styles.InputBox} placeholder="Type your location"></TextInput>
+            <View style={{flexDirection: "row"}}>
+                <View style={{width: "45%"}}>
+                    <Text preset="paragraph">From Date</Text>
+                    <TextInput style={styles.InputBox} placeholder="Type From Date"></TextInput>
+                </View>
+                <View style={{width: "45%", marginLeft: "10%"}}>
+                    <Text preset="paragraph">To Date</Text>
+                    <TextInput style={styles.InputBox} placeholder="Type To Date"></TextInput>
+                </View>
+            </View>
+            <Text preset="paragraph">Location</Text>
+            <TextInput style={styles.InputBox} placeholder="Type your location"></TextInput>
 
-          <Text preset="paragraph">To Date</Text>
-          <TextInput style={styles.InputBox} placeholder="Type your location"></TextInput>
-
-          <Text preset="paragraph">Location</Text>
-          <TextInput style={styles.InputBox} placeholder="Type your location"></TextInput>
-
-          <Text preset="paragraph">Reason</Text>
-          <TextInput style={styles.InputBox} placeholder="Type your Reason"></TextInput>
+            <Text preset="paragraph">Reason</Text>
+            <TextInput style={styles.InputBox} placeholder="Type your Reason"></TextInput>
+            
+            <Pressable style={{flexDirection: "row", alignItems: "center"}}>
+              <Image source={require('../image/upload.png')}/>
+              <Text preset="paragraph" style={{marginLeft: 10}}>File Upload</Text>
+            </Pressable>
         </View>
-
         <Button title="Apply" color="#299647"/>
     </View>
   )
